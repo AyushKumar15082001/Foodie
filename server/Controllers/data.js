9e449b26ab718b446ee5c720aa90585c3fbf87d7
Ayush Kumar <ayush.k1501@gmail.com>
2023-09-08 09:13:04 +0530

    path refactored

--- server/Controllers/data.js
+++ server/Controllers/data.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path');
-const data = fs.readFileSync('../server/data.json', 'utf8');
+// const data = fs.readFileSync('../server/data.json', 'utf8');
+const data = fs.readFileSync(path.join(__dirname, '../data.json'), 'utf8');
 exports.dataController = (req, res) => {
     console.log("data", data)
     res.json(JSON.parse(data))
